04f70774b189e42ca437690ed7901fed96762304
demiglace0505 <demiglace0505@gmail.com>
2020-03-24 20:07:23 +0800

    Started working on part 4

---
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Person.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kuchai
+ */
+public class Person {
+    private String name;
+    private int age;
+    private int weight;
+    private int height;
+    
+    public Person(String initialName){
+        this.age = 0;
+        this.name = initialName;
+        this.weight = 0;
+        this.height = 0;
+    }
+    
+    public void setHeight(int newHeight){
+        this.height = newHeight;
+    }
+    
+    public void setWeight(int newWeight){
+        this.weight = newWeight;
+    }
+    
+    public double bodyMassIndex(){
+        double heightPerHundred = this.height/100.0;
+        return this.weight / (heightPerHundred * heightPerHundred);
+    }
+    
+    public void printPerson(){
+        System.out.println(this.name + ", age " + this.age + " years");
+    }
+    
+    public void growOlder(){
+        this.age = this.age + 1;
+    }
+    
+    public int returnAge() {
+        return this.age;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public boolean isOfLegalAge(){
+        if (this.age >= 18) {
+            return true;
+        }
+        return false;
+    }
+    
+    public String toString() {
+        return this.name + ", age " + this.age + " years, body mass index is " + bodyMassIndex();
+    }
+    
+    
+    
+}
+
+
--- mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ mooc-java-programming-i/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -3,6 +3,52 @@ public class Sandbox {
 
     public static void main(String[] args) {
         // Write your program here
+        Person ichika = new Person("Ichika");
+        Person nino = new Person("Nino");
+        Person miku = new Person("Miku");
+        Person yotsuba = new Person("Yotsuba");
+        Person itsuki = new Person("Itsuki");
+
+        ichika.printPerson();
+        nino.printPerson();
+        miku.printPerson();
+        yotsuba.printPerson();
+        itsuki.printPerson();
+
+        ichika.growOlder();
+        ichika.growOlder();
+
+        for (int i = nino.returnAge(); i < 20; i++) {
+            nino.growOlder();
+        }
         
+        nino.setHeight(159);
+        nino.setWeight(50);
+        
+        System.out.println("");
+        System.out.println("Using toString:");
+        System.out.println(nino);
+
+        System.out.println("Using getName():");
+        if (ichika.isOfLegalAge()) {
+            System.out.print(ichika.getName() + " is of legal age");
+        } else {
+            System.out.print(ichika.getName() + " is underage");
+        }
+
+        System.out.println("");
+
+        if (nino.isOfLegalAge()) {
+            System.out.print(nino.getName() + " is of legal age: ");
+        } else {
+            System.out.print(nino.getName() + " is underage");
+        }
+        System.out.println("");
+        
+        //BMI
+        itsuki.setWeight(50);
+        itsuki.setHeight(159);
+        
+        System.out.println(itsuki.getName() + ", BMI is " + itsuki.bodyMassIndex());
     }
 }
